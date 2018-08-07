7aa4bf8c7cbf04a9b1772279b13d084989277e98
lovelight24 <lovelight.24@gmail.com>
2018-08-07 19:02:26 +0530

    Matplotlib

---
+++ numpyEx/numpy2.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#x = np.arange(0,3*np.pi,0.1)
+#y = np.sin(x)
+
+#plt.plot(x,y)
+#plt.show()
+
+x = np.array([1,2,3])
+#print(np.exp(x))
+print(np.log10(x))
+
+
+#sin
+#cos
+#tan
+#exp
+#log
