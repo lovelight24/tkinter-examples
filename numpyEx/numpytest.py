1a448652c3500dbd699da3a5a202c7bb0282d773
lovelight24 <lovelight.24@gmail.com>
2018-08-07 18:03:22 +0530

    Add Numpy Examples

---
+++ numpyEx/numpytest.py
@@ -0,0 +1,60 @@
+import numpy as np
+import time
+import sys
+
+#a = np.array({(1,2,3),(4,5,6)})
+#print(a)
+
+#S = range(1000)
+#print (int(sys.getsizeof(S)*len(S)))
+
+#D = np.arange(1000)
+#print(int(D.size*D.itemsize))
+
+#SIZE = 1000000
+
+#L1 = range(SIZE)
+#L2 = range(SIZE)
+
+#A1 = np.arange(SIZE)
+#A2 = np.arange(SIZE)
+
+#start = time.time()
+#result = [(x,y) for x,y in zip(L1,L2)]
+#print((time.time()-start)*1000)
+
+#start = time.time()
+#result = A1+A2
+
+
+#print((time.time()-start)*1000)
+
+#a = np.linspace(1,3,10)
+#print(a)
+
+
+#itemsize
+#size
+#reshap
+#slicing
+#max
+#RC
+#axis(R=1, C=0)
+#sum
+#Sqrt
+#std
+#vstack
+#hstack
+#ravel()
+#sin
+#cos
+#tan
+#cosin
+
+#a = np.array([(1,2,3),(4,5,6)])
+#b = np.array([(7,8,9), (5,4,3)])
+#print(np.sqrt(a))
+#print(np.std(a))
+#print(np.vstack(a+b))
+#print(np.hstack(a+b))
+#print(a.ravel())
